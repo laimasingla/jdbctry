999efc9010928d0922ced6ae6032a6d765e6855d
laimasingla <laimasingla20@gmail.com>
2019-10-16 18:55:27 +0530

    Add files via upload

---
+++ practice/src/main/java/com/cg/ibs/cardmanagement/dao/SqlQueries.java
@@ -0,0 +1,6 @@
+package com.cg.ibs.cardmanagement.dao;
+
+public class SqlQueries {
+
+	public static final String SELECT_DATA_FROM_CREDIT_CARD ="select credit_card_num, credit_card_status, name_on_cred_card, credit_cvv_num, credit_card_status, credit_expiry_date, UCI, credit_card_type, credit_score, credit_limit, income from credit_card";
+}
